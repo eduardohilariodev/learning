0f0324d84a7e7db0a7734dc14e91eed77f0cdc5e
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-05-27 12:15:56 -0300

    feat: Add DOM manipulation in assignment.js for task completion

---
+++ javascript/Academind/JavaScript - The Complete Guide 2024/dom-assignment-start/assignment.js
@@ -0,0 +1,14 @@
+const taskOne = document.querySelector("li#task-1");
+
+taskOne.style.backgroundColor = "black";
+taskOne.style.color = "white";
+
+const head = document.head;
+
+const title = head.querySelector("title");
+
+title.textContent = "Assignment - Solved!";
+
+const h1 = document.querySelector("h1");
+
+h1.textContent = "Assignment - Solved!";
