77089642b2dc54c1383f6a41d1a24a7eea556a75
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-05-31 01:18:59 -0300

    create hello world

---
+++ web/freecodecamp/go-and-react-full-stack-app-go-tutorial-for-node-developers/hello_world.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func hello_world(){
+	fmt.Println("Hello world")
+
+	// Variable declaration
+	var myName string = "John Doe"
+
+	// Constant declarion
+	const mySecondName string = "Jane Doe"
+
+	// Inferred var type declaration
+	myThirdName := "Bob Doe"
+
+	fmt.Println(myName)
+	fmt.Println(mySecondName)
+	fmt.Println(myThirdName)
+}
