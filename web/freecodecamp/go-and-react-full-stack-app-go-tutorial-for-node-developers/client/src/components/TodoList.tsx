4ad543bbb1321c046b32deba9922d3cd2a53448d
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-06-02 16:53:10 -0300

    feat: Add BASE_URL constant to fetch todos

--- web/freecodecamp/go-and-react-full-stack-app-go-tutorial-for-node-developers/client/src/components/TodoList.tsx
+++ web/freecodecamp/go-and-react-full-stack-app-go-tutorial-for-node-developers/client/src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
+import { BASE_URL } from "../contants";
 import TodoItem from "./TodoItem";
 
 export type Todo = {
@@ -17,7 +18,7 @@ const TodoList = () => {
     queryKey: ["todos"],
     queryFn: async () => {
       try {
-        const response = await fetch("http://localhost:4000/api/todos");
+        const response = await fetch(BASE_URL + "/todos");
         const data = await response.json();
 
         if (!response.ok) {
