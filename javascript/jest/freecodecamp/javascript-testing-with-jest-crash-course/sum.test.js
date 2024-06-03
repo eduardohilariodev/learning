575572a8526a446e8f45b2c19666f1ec89c4ef80
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-06-02 22:50:18 -0300

    test: update code with ES module syntax and matchers

--- javascript/jest/freecodecamp/javascript-testing-with-jest-crash-course/sum.test.js
+++ javascript/jest/freecodecamp/javascript-testing-with-jest-crash-course/sum.test.js
@@ -1,5 +1,39 @@
-const sum = require("./sum");
+const { sum } = require("./sum");
 
+/* -------------------------------------------------------------------------- */
+/*                                  MATCHERS                                  */
+/* -------------------------------------------------------------------------- */
+
+/* --------------------------------- TOBE() --------------------------------- */
 test("add 1 + 2 to equal 3", () => {
+  /** `.toBe()` is a **matcher** */
   expect(sum(1, 2)).toBe(3);
+});
+
+test("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
+});
+
+/* -------------------------------- TOEQUAL() ------------------------------- */
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+/* ---------------------------------- FALSY --------------------------------- */
+test("should return falsy when checking null", () => {
+  const foo = null;
+  expect(foo).toBeFalsy();
+});
+
+test("should return falsy when checking zero", () => {
+  const foo = 0;
+  expect(foo).toBeFalsy();
+});
+
+/* --------------------------------- TRUTHY --------------------------------- */
+test("should return truthy when checking a positive integer", () => {
+  const foo = 1;
+  expect(foo).toBeTruthy();
 });
