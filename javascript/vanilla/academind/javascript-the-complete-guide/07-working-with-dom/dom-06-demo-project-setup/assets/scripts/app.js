82fd6a85430c283c4c53e08533c31ebf90547c76
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-06-06 15:21:25 -0300

    feat: Add initial React project setup files

--- javascript/vanilla/academind/javascript-the-complete-guide/07-working-with-dom/dom-06-demo-project-setup/assets/scripts/app.js
+++ javascript/vanilla/academind/javascript-the-complete-guide/07-working-with-dom/dom-06-demo-project-setup/assets/scripts/app.js
@@ -2,6 +2,10 @@ const backdrop = document.getElementById("backdrop");
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -12,13 +16,50 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
 };
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    Number(ratingValue) < 1 ||
+    Number(ratingValue) > 5
+  ) {
+    alert("Please enter a rating between 1 and 5");
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.debug(movies);
+  toggleMovieModal();
+  
+};
+
+const clearMovieInput = () => {
+  for (const userInput of userInputs) {
+    userInput.value = "";
+  }
+};
+
 const backdropClickHandler = () => {
   toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
 backdrop.addEventListener("click", toggleMovieModal);
-cancelAddMovieButton.addEventListener("click", cancelAddMovie);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
