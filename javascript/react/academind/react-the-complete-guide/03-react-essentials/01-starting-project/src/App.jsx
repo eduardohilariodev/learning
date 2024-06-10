2f7efdf0646c57c7fdcf577dafea9ca3b00a745c
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-06-10 11:09:22 -0300

    feat: Add CoreConcept component and CORE_CONCEPTS data

--- javascript/react/academind/react-the-complete-guide/03-react-essentials/01-starting-project/src/App.jsx
+++ javascript/react/academind/react-the-complete-guide/03-react-essentials/01-starting-project/src/App.jsx
@@ -1,23 +1,38 @@
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
 
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * (max + 1));
 };
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img
+        src={props.img}
+        alt={props.title}
+        srcset=""
+      />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function Header() {
   const description = reactDescriptions[getRandomInt(2)];
-
   return (
     <header>
       <img
-        src="src/assets/react-core-concepts.png"
+        src={reactImg}
         alt="Stylized atom"
       />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
         going to build!
-      </p>
+      </p>{" "}
     </header>
   );
 }
@@ -26,12 +41,34 @@ function App() {
   return (
     <div>
       <Header />
-
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              img={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              img={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              img={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              img={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
-
 export default App;
