542b7f6064626ae6a498eec854f49ccdda2cf8d2
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-06-10 11:12:00 -0300

    refactor: Update CoreConcept component props to use object destructuring

--- javascript/react/academind/react-the-complete-guide/03-react-essentials/01-starting-project/src/App.jsx
+++ javascript/react/academind/react-the-complete-guide/03-react-essentials/01-starting-project/src/App.jsx
@@ -6,16 +6,16 @@ const getRandomInt = (max) => {
   return Math.floor(Math.random() * (max + 1));
 };
 
-function CoreConcept(props) {
+function CoreConcept({ image, title, description }) {
   return (
     <li>
       <img
-        src={props.img}
-        alt={props.title}
+        src={image}
+        alt={title}
         srcset=""
       />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -45,26 +45,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              img={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              img={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              img={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              img={CORE_CONCEPTS[3].image}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
