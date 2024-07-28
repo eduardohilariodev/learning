f4734b56869ef71cc2288971f0e1f4e2b079ef8c
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-07-28 20:36:57 -0300

    192

--- javascript/vanilla/academind/javascript-the-complete-guide/07-arrays-iterables/script.js
+++ javascript/vanilla/academind/javascript-the-complete-guide/07-arrays-iterables/script.js
@@ -5,10 +5,14 @@
 // hobbies.shift();
 // console.debug(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const testResults = [1, 5.3, 1.5, 10.99, -5, 1.5, 10];
 
 const storedResults = testResults.concat([3.99, 2]);
 
 testResults.push(5.91);
 
 console.debug(storedResults, testResults);
+console.debug(testResults.lastIndexOf(1.5));
+
+const personData = [{ name: "Eduardo" }, { name: "Manuel" }];
+console.debug(personData.indexOf({ name: "Manuel" }));
