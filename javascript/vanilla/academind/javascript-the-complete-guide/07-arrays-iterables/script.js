7abd90d83270274a7d9f9d0a0e59a117defde3b0
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-07-28 21:13:46 -0300

    197

--- javascript/vanilla/academind/javascript-the-complete-guide/07-arrays-iterables/script.js
+++ javascript/vanilla/academind/javascript-the-complete-guide/07-arrays-iterables/script.js
@@ -36,19 +36,40 @@
 
 // console.debug(maxIndex);
 
-const prices = [10.99, 5.99, 3.99, 6.59];
+// const prices = [10.99, 5.99, 3.99, 6.59];
 
+// const tax = 0.19;
+
+// const taxAdjustedPrices = [];
+
+// // for (const price of prices) {
+// //   taxAdjustedPrices.push(price * (1 + tax));
+// // }
+
+// prices.forEach((price, index, prices) => {
+//   const priceObj = { index, taxAdjustedPrices: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
+
+// console.debug(taxAdjustedPrices);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
-const taxAdjustedPrices = [];
-
-// for (const price of prices) {
-//   taxAdjustedPrices.push(price * (1 + tax));
-// }
-
-prices.forEach((price, index, prices) => {
+const taxAdjustedPrices = prices.map((price, index, prices) => {
   const priceObj = { index, taxAdjustedPrices: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
 console.debug(taxAdjustedPrices);
+
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return -1;
+  }
+});
+console.debug(sortedPrices.reverse());
