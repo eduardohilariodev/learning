cc24c8d175faec37acd21955300f3824bd05ac44
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-09-02 18:22:36 -0300

    react/77

--- javascript/react/academind/react-the-complete-guide/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ javascript/react/academind/react-the-complete-guide/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+  };
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? (
+          <input
+            required
+            value={name}
+            type="text"
+            name=""
+            id=""
+          />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 };
