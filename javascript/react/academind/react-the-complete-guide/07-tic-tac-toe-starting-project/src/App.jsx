fc8775cf0e6f8eae5597e38963851e93b60a0904
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-09-03 16:19:06 -0300

    react/82

--- javascript/react/academind/react-the-complete-guide/07-tic-tac-toe-starting-project/src/App.jsx
+++ javascript/react/academind/react-the-complete-guide/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 export default function App() {
+  const [activePlayer, setActivePlayer] = useState();
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <div id="game-container">
-      <ol id="players">
+      <ol
+        id="players"
+        className="highlight-player"
+      >
         <Player
           initialName="Player 1"
           symbol="X"
+          isActive={activePlayer === "X"}
         />
         <Player
           initialName="Player 2"
           symbol="O"
+          isActive={activePlayer === "O"}
         />
       </ol>
-      <GameBoard />
+      <GameBoard
+        onSelectSquare={handleSelectSquare}
+        activePlayerSymbol={activePlayer}
+      />
     </div>
   );
 }
