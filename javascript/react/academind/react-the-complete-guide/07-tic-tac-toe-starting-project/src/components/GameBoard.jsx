480603831cd0449e39987d2403aac80583d35443
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-09-04 15:15:14 -0300

    react-the-complete-guide/88

--- javascript/react/academind/react-the-complete-guide/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ javascript/react/academind/react-the-complete-guide/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -38,6 +38,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
                 <button
                   type="button"
                   onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
                 >
                   {playerSymbol}
                 </button>
