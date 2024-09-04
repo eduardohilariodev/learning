c479a725a6fb1ecb04cfbc93997eaa335692d7b1
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-09-04 16:10:51 -0300

    react-the-complete-guide/95

--- javascript/react/academind/react-the-complete-guide/07-tic-tac-toe-starting-project/src/App.jsx
+++ javascript/react/academind/react-the-complete-guide/07-tic-tac-toe-starting-project/src/App.jsx
@@ -49,7 +49,7 @@ export default function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -87,11 +87,13 @@ export default function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
