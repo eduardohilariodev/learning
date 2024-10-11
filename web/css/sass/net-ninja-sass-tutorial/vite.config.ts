d50b8c448be82d4c622a19731179373e242915e5
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-10-11 03:31:35 -0300

    SASS Tutorial (build your own CSS library) #11 - Parent Selectors

--- web/css/sass/net-ninja-sass-tutorial/vite.config.ts
+++ web/css/sass/net-ninja-sass-tutorial/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    emptyOutDir: true,
+    emptyOutDir: false,
     outDir: "dist",
     cssMinify: false,
     rollupOptions: {
