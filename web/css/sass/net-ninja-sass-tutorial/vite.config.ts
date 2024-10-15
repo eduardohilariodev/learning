31ad16edfb9960e76ca9c31964b18c46452930ec
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-10-15 16:28:58 -0300

    SASS Tutorial (build your own CSS library) #18 - Using @extend

--- web/css/sass/net-ninja-sass-tutorial/vite.config.ts
+++ web/css/sass/net-ninja-sass-tutorial/vite.config.ts
@@ -2,13 +2,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    emptyOutDir: false,
-
-    outDir: "dist",
     cssMinify: false,
-    rollupOptions: {
-      output: { assetFileNames: "[name].[ext]" },
-    },
+    emptyOutDir: false,
+    outDir: "dist",
+    rollupOptions: { output: { assetFileNames: "[name].[ext]" } },
   },
   css: { preprocessorOptions: { scss: { api: "modern-compiler" } } },
 });
