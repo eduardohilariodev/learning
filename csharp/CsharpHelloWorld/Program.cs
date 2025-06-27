ff68ac9d04b4b8d208a8207965aa5ae581e8821d
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2025-06-27 16:10:52 -0300

    Update character details in Main method of C# console application

--- csharp/CsharpHelloWorld/Program.cs
+++ csharp/CsharpHelloWorld/Program.cs
@@ -11,10 +11,14 @@ namespace CsharpHelloWorld
         static void Main()
         {
             string characterName = "George";
-            int characterAge = 70;
+            int characterAge = 35;
 
             Console.WriteLine("There once was a man named " + characterName);
             Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            characterAge = 25;
+
             Console.WriteLine("He really liked the name " + characterName);
             Console.WriteLine("But didn't like being " + characterAge);
 
