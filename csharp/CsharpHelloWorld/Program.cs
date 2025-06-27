2ffaf70e146c3cf12e6e4735201e45de4d03ea0b
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2025-06-27 16:05:54 -0300

    Refactor Main method in C# console application to introduce character details and remove ASCII art

--- csharp/CsharpHelloWorld/Program.cs
+++ csharp/CsharpHelloWorld/Program.cs
@@ -8,14 +8,17 @@ namespace CsharpHelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            string characterName = "George";
+            int characterAge = 70;
 
-            Console.ReadLine();
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            // Console.ReadLine();
         }
     }
 }
